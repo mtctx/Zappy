d020d831067a2ea1854ff30bb7bf0088a9a5f22d
mtctx <me@mtctx.dev>
2025-10-30 17:13:36 +0100

    Changed example

--- test/src/main/kotlin/dev/mtctx/zappy/test/Main.kt
+++ test/src/main/kotlin/dev/mtctx/zappy/test/Main.kt
@@ -16,9 +16,46 @@
 
 package dev.mtctx.zappy.test
 
+import dev.mtctx.zappy.annotation.*
 import dev.mtctx.zappy.mock
+import dev.mtctx.zappy.zpl.ZPLProvider
 
+object TestZplProvider : ZPLProvider() {
+    override val id: String = "test"
+    override val characterList = ('a'..'z') + ('A'..'Z') + ('0'..'9') + listOf('-', '_', '=')
+}
+
+@ZappyAnnotation
+annotation class TestAnno(val zpl: String = "<test>")
+
+/**
+ * | ID               | Example Output                         |
+ * |------------------|----------------------------------------|
+ * | `<name>`     | `k9PxM2vN`                             |
+ * | `<email>`        | `k9PxM2vN@aa2fa5.org`                  |
+ * | `<domain>`       | `aa2fa5.net`                           |
+ * | `<numeric>`      | `42`                                   |
+ * | `<password>`     | `p@ssW0rd!`                            |
+ * | `<token>`        | `aB9-xY2_zW8=`                         |
+ * | `<uuid>`         | `f47ac10b-58cc-4372-a567-0e02b2c3d479` |
+ * | `<iso-date>`     | `2025-10-29T14:30:22Z`                 |
+ * | `<phone-number>` | `+15551234567`                         |
+ * | `<url>`          | `https://example.com`                  |
+ */
+@Mock
+data class Test(
+    @TestAnno val test: String,
+    @Name val name: String,
+    @Email val email: String,
+    @Domain val domain: String,
+    @Numeric val age: String,
+    @Password val password: String,
+    @Token val token: String,
+    @UUID val uuid: String,
+    @PhoneNumber val phoneNumber: String,
+    @URL val url: String,
+)
 
 fun main() {
-    println(mock<Test>())
+    println(mock<Test>(TestZplProvider))
 }
